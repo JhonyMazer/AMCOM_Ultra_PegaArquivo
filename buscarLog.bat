df8ed7a1fca168d94bea2748226d49162c6155f1
jhonymazer <92GHG9bCnjhnp@Q>
2024-08-22 14:27:23 -0300

    ajuste diretorio de execucao pelo bat

--- buscarLog.bat
+++ buscarLog.bat
@@ -1,4 +1,4 @@
 
 cd C:\Users\jhony.tmazer\Desktop\LOCAL\PYTHON\AMCOM_Ultra_PegaArquivo
-C:\Users\jhony.tmazer\AppData\Local\Programs\Python\Python310\python.exe -m main.py
+C:\Users\jhony.tmazer\Desktop\LOCAL\PYTHON\AMCOM_Ultra_PegaArquivo\venv\Scripts\python.exe -m main.py
 pause
